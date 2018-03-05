7b13221c5444bbec60690bcd13a0bb23f4227b72
jacobbrendall <alibaba@alis-mbp.fios-router.home>
2018-03-05 11:39:19 -0500

    hi there

---
+++ src/Overriding_vs_Overloading/Cat.java
@@ -0,0 +1,13 @@
+package Overriding_vs_Overloading;
+
+public class Cat extends Animal{
+public static void main(String[] args){
+	Cat a=new Cat();
+	a.bark(str);
+	
+}
+	public void bark(String str){
+	 System.out.println("Cat is barking");
+}
+
+}
